da1af442a9ab701fb026e05bc9bfe85247c1ba07
koleso_O <kolesnikovfd@gmail.com>
2024-02-12 18:54:56 +0400

    тренировки в полете

--- 1task.py
+++ 1task.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, render_template
 
 
 app = Flask(__name__)
@@ -8,6 +8,11 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/index')
 def index():
     return render_template('index.html', title='Домашняя страница')
+
+
+@app.route('/training/<prof>')
+def image(prof):
+    return render_template('index2.html', prof=prof)
 
 
 if __name__ == '__main__':
